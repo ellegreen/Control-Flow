641b26b048f8ed4c8349880c8e760fda20657cc9
Elle Green <02elle.green@gulllakecs.org>
2020-01-06 08:38:01 -0500

    Add running totals

--- ControlFlow.py
+++ ControlFlow.py
@@ -2,5 +2,15 @@
 Programmer: Elle green
 Date: 1-6-20
 Program: Running Total
+
+This program asks users for 5 numbers
+It then sums up the products
 """
 
+sum = 0
+
+for i in range(5):
+    enter_a_number = int(input('Enter a Number: '))
+    sum = sum + enter_a_number
+
+print('\nThe sum of your numbers is: ' + str(sum))
