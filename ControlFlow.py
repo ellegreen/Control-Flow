5bec400f12d1c0d57c164c7800d667b7656ea783
Elle Green <02elle.green@gulllakecs.org>
2020-01-09 07:55:03 -0500

    Add round and update format

--- ControlFlow.py
+++ ControlFlow.py
@@ -5,7 +5,7 @@ Program: Running Total
 
 This program asks users for 5 numbers
 It then sums up the products
-
+"""
 
 sum = 0
 how_many_numbers = int(input('How many numbers would you like to sum up: '))
@@ -15,7 +15,7 @@ for i in range(how_many_numbers):
     sum = sum + enter_a_number
 
 print('\nThe sum of your numbers is: ' + str(sum))
-"""
+
 
 """
 Programmer: Elle Green
