96937dc463e57af25d2154c2208037b9d1745199
Elle Green <02elle.green@gulllakecs.org>
2020-01-23 08:27:42 -0500

    Add Double For Loop
    We created an Outer and Inner for loops

---
+++ ControlFlow.py
@@ -0,0 +1,8 @@
+# Programmer: Elle Green
+# Date: 1-20-20
+# Program: Double For Loop
+
+for i in range(3):
+    print("Outer For Loop " + str(i))
+    for k in range(4):
+        print("\tInner for Loop " + str(k))
