74eab629eeebd682e5fd9c9e84362e51bbd3e75f
Elle Green <02elle.green@gulllakecs.org>
2020-02-04 07:48:02 -0500

    Functions and Variables

--- ControlFlow.py
+++ ControlFlow.py
@@ -1,9 +1,20 @@
 # Date 2-3-20
 # Greeting Function
 
+# Declare Global Variables
 name = input('\nWhat is your name: ')
+x = 15
 
+# Create Functions here
 def greeting():
     print('Hi there ' + name)
+    print(x)
 
+def printSomething():
+    x = 3
+    print(x)
+
+# Call Functions here
 greeting()
+printSomething()
+print(x)
